e7fa4698640df5e8b1573b7c5e419f6a50e912e1
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-01 16:06:22 +0000

    Added custom logos and setup login page markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Login from './Login'
 
 function App() {
   return (
     <div className="app">
-		<h1>spotify clone </h1>            
+		<Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.scss';
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="./spotify-192.png" alt="Spotify logo" />
+            <h1>Spotify</h1>
+            <a href="" title="Login with Shopify">LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
