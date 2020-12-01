b8ff6ec228f1d2dffa2b6f9a8d097f76f9212b68
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-01 21:08:40 +0000

    Built header comp, search and user avatar/name

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Body.scss';
+import Header from './Header';
 
-function Body() {
+function Body({ spotify }) {
     return (
         <div className="body">
-            <h1>body comp</h1>
+            <Header spotify={spotify}/>
         </div>
     )
 }
