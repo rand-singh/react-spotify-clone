9fc6263844f9d4256ec5a785741c1669ddd62c3a
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-01 17:02:53 +0000

    After login fetch token and render different message on screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  return (
-    <div className="app">
-		<Login />
-    </div>
-  );
+	const [token, setToken] = useState(null);
+
+	// run code based on a given condition
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = "";
+		const _token = hash.access_token;
+
+		if (_token) {
+			setToken(_token);
+		}
+
+	}, []);
+
+	return (
+		<div className="app">
+			{
+				token ? (
+					<code>logged in successfully</code>
+				) : (
+					<Login />
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Login.scss';
+import { loginUrl } from './spotify';
 
 function Login() {
     return (
         <div className="login">
             <img src="./spotify-192.png" alt="Spotify logo" />
             <h1>Spotify</h1>
-            <a href="" title="Login with Shopify">LOGIN WITH SPOTIFY</a>
+            <a href={loginUrl} title="Login with Shopify">LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
